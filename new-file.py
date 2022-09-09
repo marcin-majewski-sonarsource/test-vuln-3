f4e44865af6b911951b6007fd1dbb9ed7a362c15
Marcin Majewski <69035061+marcin-majewski-sonarsource@users.noreply.github.com>
2022-09-09 12:41:41 +0200

    New commit

--- new-file.py
+++ new-file.py
@@ -6,7 +6,7 @@ print 'test-2'
 @app.route('/')
 def index():
     module = request.args.get("module")
-            exec("import urllib%s as urllib" % module) # Noncompliant\
+            exec("changed code ort urllib%s as urllib" % module) # Noncompliant\
     
 @app.route('/')
 def index2():
